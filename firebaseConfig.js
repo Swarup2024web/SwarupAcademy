821b4ff2d4f95dc8de491e9c46c313bf890637fd
Swarup2024web <bswarup1998@gmail.com>
2025-07-13 18:33:21 +0530

    Create firebaseConfig.js

---
+++ firebaseConfig.js
@@ -0,0 +1,9 @@
+// firebaseConfig.js
+export const firebaseConfig = {
+  apiKey: "AIzaSyCgTCmi2smeMCHNR1OeZ0lAXjuswNzJcck",
+  authDomain: "swarupacademy-e9d6c.firebaseapp.com",
+  projectId: "swarupacademy-e9d6c",
+  storageBucket: "swarupacademy-e9d6c.firebasestorage.app",
+  messagingSenderId: "1060893563837",
+  appId: "1:1060893563837:web:a2cbf448dc80f4be9f90d4"
+};
